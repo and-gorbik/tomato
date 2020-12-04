ab59b16c85131fbc2fac715a77498871d24271dc
and-gorbik <and.gorbik@gmail.com>
2020-12-04 21:06:12 +0300

    update sql migrations and queries

--- server/repo/queries.go
+++ server/repo/queries.go
@@ -1,11 +1,63 @@
 package repo
 
 const (
-	getTasksQuery          = `SELECT title, tag.name, finish_dt FROM task INNER JOIN tag USING tag_id;`
-	saveTaskQuery          = `INSERT INTO task(title, tag, finish_dt) VALUES (?, ?, ?);`
-	getCurrentTaskQuery    = `SELECT title, tag.name, start_dt FROM current_task INNER JOIN tag USING tag_id;`
-	addCurrentTaskQuery    = `INSERT INTO current_task(title, tag_id, start_dt) VALUES (?, ?, ?);`
-	deleteCurrentTaskQuery = `DELETE FROM current_task;`
-	addTagQuery            = `INSERT INTO tag(name) VALUES (?);`
-	getTagQuery            = `SELECT tag_id FROM tag WHERE name = ?;`
+	sqliteQueryGetTasks = `
+	SELECT title, tag.name, finish_dt FROM task
+	JOIN user USING (user_id)
+	LEFT OUTER JOIN tag USING (tag_id)
+	WHERE user.name = ?;
+	`
+)
+
+const (
+	sqliteQueryAddTask = `
+	INSERT INTO task(title, finish_dt, tag_id, user_id)
+	VALUES(?, ?, ?, ?);
+	`
+)
+
+const (
+	sqliteQueryGetCurrentTask = `
+	SELECT title, tag.name, start_dt FROM current_task
+	JOIN user USING (user_id)
+	LEFT OUTER JOIN tag USING (tag_id)
+	WHERE user.name = ?;
+	`
+)
+
+const (
+	sqliteQueryAddCurrentTask = `
+	INSERT INTO current_task(title, start_dt, tag_id, user_id)
+	VALUES(?, ?, ?, ?);
+	`
+)
+
+const (
+	sqliteQueryDeleteCurrentTask = `
+	DELETE FROM current_task WHERE user_id = ?;
+	`
+)
+
+const (
+	sqliteQueryAddTagQuery = `
+	INSERT OR IGNORE INTO tag(name) VALUES(?);
+	`
+)
+
+const (
+	sqliteQueryGetTagIDQuery = `
+	SELECT tag_id FROM tag WHERE name = ?;
+	`
+)
+
+const (
+	sqliteQueryAddUserQuery = `
+	INSERT OR IGNORE INTO user(name) VALUES(?);
+	`
+)
+
+const (
+	sqliteQueryGetUserIDQuery = `
+	SELECT user_id FROM user WHERE name = ?;
+	`
 )
--- sql/migration.sql
+++ sql/migration.sql
@@ -1,16 +1,29 @@
-CREATE TABLE IF NOT EXISTS WITHOUT ROWID tag (
-    tag_id INT NOT NULL AUTOINCREMENT,
+PRAGMA foreign_keys = 1;
+
+CREATE TABLE IF NOT EXISTS user (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS tag (
+    tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS task (
     title VARCHAR(100) NOT NULL,
-    finish_date DATETIME NOT NULL,
-    tag_id INT NOT NULL
+    finish_dt DATETIME NOT NULL,
+    tag_id INTEGER,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (tag_id) REFERENCES tag (tag_id),
+    FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS current_task (
     title VARCHAR(100) NOT NULL,
-    start_date DATETIME NOT NULL,
-    tag_id INT NOT NULL
+    start_dt DATETIME NOT NULL,
+    tag_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY (tag_id) REFERENCES tag (tag_id),
+    FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
